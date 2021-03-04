8ac6b3a8e1eecc36ce09709b5509f53407a7e320
januxdesa <januxdesa@users.noreply.github.com>
2021-03-04 12:28:01 -0300

    JUNIT 2.- Model Test (basado en BOOK)

---
+++ Book/src/main/java/com/codingdojo/web/models/Author.java
@@ -0,0 +1,80 @@
+package com.codingdojo.web.models;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name = "authors")
+public class Author {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@NotBlank
+	@NotNull
+	@Size(min = 2)
+	private String firstName;
+	@NotBlank
+	@NotNull
+	@Size(min = 2)
+	private String lastName;
+	@NotBlank
+	@NotNull
+	@Size(min = 5)
+	private String description;
+
+	@Column(updatable = false)
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date createdAt;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date updatedAt;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	
+	
+}
--- Book/src/test/java/com/codingdojo/web/BookApplicationTests.java
+++ Book/src/test/java/com/codingdojo/web/BookApplicationTests.java
@@ -1,13 +1,100 @@
 package com.codingdojo.web;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.codingdojo.web.controllers.BooksController;
+import com.codingdojo.web.models.Author;
+import com.codingdojo.web.models.Book;
 
 @SpringBootTest
 class BookApplicationTests {
 
+	@Autowired
+    // use the name of the controller you create in your project
+    private BooksController controller;
+	
+	private static Validator validator;
+    
+	@BeforeAll
+	static void setUp() {
+	    ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+	    validator = factory.getValidator();
+	}
+	
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+    void testController() {
+        assertThat(controller).isNotNull();
+    }
+	
+	@Test
+	void assumeFirstNameisEmpty() {
+	   // Book book = new Book();
+	    Author author = new Author(); 
+	    author.setFirstName("");
+	    author.setLastName("Test");
+	    author.setDescription("Description");
+	    Set<ConstraintViolation<Author>> violations = validator.validate(author);
+	   // assertFalse(violations.isEmpty());
+	    assertTrue(violations.isEmpty());
+	}
+	@Test
+	void assumeFirstNameIsSizeMinor() {
+	   // Book book = new Book();
+	    Author author = new Author(); 
+	    author.setFirstName("P");
+	    author.setLastName("Test");
+	    author.setDescription("Description");
+	    Set<ConstraintViolation<Author>> violations = validator.validate(author);
+	    assertTrue(violations.isEmpty());
+	}
+	
+	@Test
+	void assumeLastNameIsNull() {
+	   // Book book = new Book();
+	    Author author = new Author(); 
+	    author.setFirstName("Test");
+	    author.setDescription("Description");
+	    Set<ConstraintViolation<Author>> violations = validator.validate(author);
+	    assertTrue(violations.isEmpty());
+	}
+	
+	
+	@Test
+	void assumeDescriptionIsSizeMinor() {
+	   // Book book = new Book();
+	    Author author = new Author(); 
+	    author.setFirstName("Pedro");
+	    author.setLastName("Test");
+	    author.setDescription("1234");
+	    Set<ConstraintViolation<Author>> violations = validator.validate(author);
+	    assertTrue(violations.isEmpty());
+	}
+	@Test
+	void assumeDescriptionLastNameIsNull() {
+	   // Book book = new Book();
+	    Author author = new Author(); 
+	    author.setFirstName("Pedro");
+	    author.setLastName("Test");
+	    Set<ConstraintViolation<Author>> violations = validator.validate(author);
+	    assertFalse(violations.isEmpty());
 	}
 
 }
